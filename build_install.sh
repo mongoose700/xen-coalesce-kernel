296405a1191116c40a3a8aefab31b387747c83c2
Kevin Boos <kevinaboos@gmail.com>
2016-04-21 23:26:31 -0500

    Fixed scripts, they should work perfectly now

--- build_install.sh
+++ build_install.sh
@@ -2,18 +2,18 @@ sudo echo "building and installing linux kernel 3.19.0"
 
 export LOCALVERSION=""
 
-make -j8 LOCALVERSION=""
+make -j16 LOCALVERSION=""
 if [ $? != 0 ]
 then
 	echo "make failed, halting build script"
 	exit 1;
 fi
 
-sudo make -j8 modules LOCALVERSION=""
-sudo make modules_install 
+make -j16 modules LOCALVERSION=""
+sudo make modules_install LOCALVERSION=""
 
 sudo rm /boot/initrd.img-3.19.0 /boot/config-3.19.0 /boot/vmlinuz-3.19.0 /boot/System.map-3.19.0
 
-sudo make install && \
+sudo make install LOCALVERSION="" && \
 sudo update-grub
 
--- single_build_install.sh
+++ single_build_install.sh
@@ -3,6 +3,7 @@ sudo echo "building and installing module in $M_TO_BUILD"
 
 export LOCALVERSION=""
 export M=$M_TO_BUILD
+export INSTALL_MOD_DIR="kernel/drivers/block/xen-blkback/"
 
 make -j8 LOCALVERSION="" M=$M_TO_BUILD modules
 if [ $? != 0 ]
@@ -11,4 +12,4 @@ then
 	exit 1;
 fi
 
-sudo make LOCALVERSION="" M=$M_TO_BUILD modules_install 
+sudo make LOCALVERSION="" M=$M_TO_BUILD INSTALL_MOD_DIR=$INSTALL_MOD_DIR modules_install 
